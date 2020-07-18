e8bbfbb6cbe091a4bda91cf8f872f340e8c75a73
Paweł Worwąg <pawel.worwag@gmail.com>
2020-07-18 21:53:18 +0200

    Add simple ring binders list

--- src/Areas/Ringbinders/Controllers/RingbinderController.cs
+++ src/Areas/Ringbinders/Controllers/RingbinderController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using DocuHub.DAL;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DocuHub.Areas.RingBinders.Controllers
@@ -8,15 +9,16 @@ namespace DocuHub.Areas.RingBinders.Controllers
     [Area("Ringbinders")]
     public class RingbinderController:Controller
     {
-        public RingbinderController()
+        private readonly IRingBindersRepository _ringbinders;
+        public RingbinderController(IRingBindersRepository ringbinders)
         {
-            
+            _ringbinders = ringbinders;
         }
 
         [HttpGet("/ringbinders")]
         public IActionResult Index()
         {
-            return View();
+            return View(_ringbinders.GetAllRingBinders());
         }
     }
 }
