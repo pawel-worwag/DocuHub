c8d9233aa091f807d7eea506eecf706532b2b5f7
Paweł Worwąg <pawel.worwag@gmail.com>
2020-07-18 16:49:57 +0200

    Create Ringbinders controller

---
+++ src/Areas/Ringbinders/Controllers/RingbinderController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocuHub.Areas.RingBinders.Controllers
+{
+    [Area("Ringbinders")]
+    public class RingbinderController:Controller
+    {
+        public RingbinderController()
+        {
+            
+        }
+
+        [HttpGet("/ringbinders")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
