6399f26dcf21b2a55f88223a274cb578a4c8b478
Paweł Worwąg <pawel.worwag@gmail.com>
2020-07-18 19:07:48 +0200

    Add loading RingBinders from the database

--- src/DAL/IRingBindersRepository.cs
+++ src/DAL/IRingBindersRepository.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using DocuHub.BOL;
 
 namespace DocuHub.DAL
 {
 
     public interface IRingBindersRepository
     {
-        
+        public List<RingBinder> GetAllRingBinders();
     }
 
 
--- src/DAL/RingBindersRepository.cs
+++ src/DAL/RingBindersRepository.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using DocuHub.BOL;
 
 namespace DocuHub.DAL
 {
     public class RingBindersRepository:IRingBindersRepository
     {
-        
-        public RingBindersRepository()
+        private readonly DocuHubDbContext _db;
+        public RingBindersRepository(DocuHubDbContext dbContext)
         {
-            
+            _db = dbContext;
         }
 
+        public List<RingBinder> GetAllRingBinders()
+        {
+            return _db.RingBinders.OrderByDescending(b => b.Pinned).ThenBy(b => b.Name).ToList();
+        }
     }
 }
