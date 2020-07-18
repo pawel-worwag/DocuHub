230e4c036dc19ef11972a86d2a473a0765a5f215
Paweł Worwąg <pawel.worwag@gmail.com>
2020-07-18 18:39:13 +0200

    Add database context pool

--- src/Startup.cs
+++ src/Startup.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocuHub.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace DocuHub
 {
@@ -28,6 +31,16 @@ namespace DocuHub
             services.AddMvc(options => {
                 options.EnableEndpointRouting = false;
             });
+
+            string connectionString = Configuration.GetValue<string>("Database:ConnectionString");
+            int major = Configuration.GetValue<int>("Database:DbVersion:Major");
+            int minor = Configuration.GetValue<int>("Database:DbVersion:Minor");
+            int build = Configuration.GetValue<int>("Database:DbVersion:Build");
+            ServerType type = Configuration.GetValue<ServerType>("Database:DbType");
+            services.AddDbContextPool<DocuHubDbContext>(options => options
+                .UseMySql(connectionString,
+                     mySqlOptions => mySqlOptions
+                     .ServerVersion(new Version(major, minor, build), type)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
